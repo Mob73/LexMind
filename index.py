79399e681aa8b987d0d3d5c9814c3170d8f2a3ff
Moubarak  Djato <piratezakstorm@gmail.com>
2026-08-28 01:10:49 +0200

    Delete index.py

--- index.py
+++
@@ -1,9 +0,0 @@
-# index.py
-from streamlit_lambda import streamlit_lambda
-from app import app as streamlit_app  # Votre instance Streamlit
-
-# Le wrapper streamlit_lambda transforme votre app en fonction handler
-lambda_app = streamlit_lambda(streamlit_app)
-
-def handler(event, context):
-    return lambda_app(event, context)
