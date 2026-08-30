18943863665b5d0f5b61371f2af9219cd91640c5
Moubarak  Djato <piratezakstorm@gmail.com>
2026-08-30 20:39:04 +0200

    Update llm_client.py

--- src/llm_client.py
+++ src/llm_client.py
@@ -5,7 +5,7 @@ import streamlit as st
 def create_llm():
 
     return ChatGoogleGenerativeAI(
-        model="gemini-3.5-flash-lite",
+        model=CONFIG["llm_model"],
         google_api_key="AQ.Ab8RN6KEOjAGdNFCtFz4fmNAppYBx8Njih9JnJbYKykT6L48NQ",
         temperature=CONFIG["llm_temperature"],
     )
